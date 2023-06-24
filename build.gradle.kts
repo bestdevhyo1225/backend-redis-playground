83000e947c97c18e7768a5960f5e08000d2e85b4
janghyoseog <bestdevhyo1225@gmail.com>
2023-06-24 13:50:23 +0900

    style: tap size: 4로 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,54 +2,54 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.springframework.boot.gradle.plugin.SpringBootPlugin
 
 plugins {
-	id("org.springframework.boot") version "3.1.0" apply false
-	id("io.spring.dependency-management") version "1.1.0"
-	kotlin("jvm") version "1.8.21"
-	kotlin("plugin.spring") version "1.8.21"
-	kotlin("kapt") version "1.8.21"
+    id("org.springframework.boot") version "3.1.0" apply false
+    id("io.spring.dependency-management") version "1.1.0"
+    kotlin("jvm") version "1.8.21"
+    kotlin("plugin.spring") version "1.8.21"
+    kotlin("kapt") version "1.8.21"
 }
 
 allprojects {
-	apply(plugin = "java")
+    apply(plugin = "java")
 
-	group = "com.hyoseok"
-	version = "0.0.1-SNAPSHOT"
-	java.sourceCompatibility = JavaVersion.VERSION_17
+    group = "com.hyoseok"
+    version = "0.0.1-SNAPSHOT"
+    java.sourceCompatibility = JavaVersion.VERSION_17
 
-	repositories {
-		mavenCentral()
-	}
+    repositories {
+        mavenCentral()
+    }
 }
 
 subprojects {
-	apply(plugin = "kotlin")
-	apply(plugin = "kotlin-spring")
-	apply(plugin = "io.spring.dependency-management")
-	apply(plugin = "kotlin-kapt")
+    apply(plugin = "kotlin")
+    apply(plugin = "kotlin-spring")
+    apply(plugin = "io.spring.dependency-management")
+    apply(plugin = "kotlin-kapt")
 
-	dependencyManagement {
-		imports {
-			mavenBom(SpringBootPlugin.BOM_COORDINATES)
-		}
-	}
+    dependencyManagement {
+        imports {
+            mavenBom(SpringBootPlugin.BOM_COORDINATES)
+        }
+    }
 
-	dependencies {
-		implementation("org.springframework.boot:spring-boot-starter-data-redis")
-		implementation("org.springframework.boot:spring-boot-starter-web")
-		implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-		implementation("org.jetbrains.kotlin:kotlin-reflect")
-		implementation("io.github.microutils:kotlin-logging:3.0.5")
-		testImplementation("org.springframework.boot:spring-boot-starter-test")
-	}
+    dependencies {
+        implementation("org.springframework.boot:spring-boot-starter-data-redis")
+        implementation("org.springframework.boot:spring-boot-starter-web")
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+        implementation("org.jetbrains.kotlin:kotlin-reflect")
+        implementation("io.github.microutils:kotlin-logging:3.0.5")
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+    }
 
-	tasks.withType<KotlinCompile> {
-		kotlinOptions {
-			freeCompilerArgs += "-Xjsr305=strict"
-			jvmTarget = "17"
-		}
-	}
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs += "-Xjsr305=strict"
+            jvmTarget = "17"
+        }
+    }
 
-	tasks.withType<Test> {
-		useJUnitPlatform()
-	}
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
 }
